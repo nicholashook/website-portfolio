07673797bff64c4a84da07803ade4cf35b3f9dd8
Ilya Petrov <ip4763397@gmail.com>
2026-03-29 16:41:36 +0800

    1.0.3 email copy confirmation

--- scripts/index.js
+++ scripts/index.js
@@ -4,9 +4,18 @@ document.body.querySelectorAll('.email-button')
   .forEach((element, index) => {
     element.addEventListener('click', () => {
       copyEmail(email);
+      confirmCopy(element);
     })
   });
 
 function copyEmail(email) {
   navigator.clipboard.writeText(email);
+}
+
+function confirmCopy(element) {
+  confirmMsg = element.querySelector('.email-confirm-off');
+  confirmMsg.classList.add('email-confirm-on');
+  setTimeout(() => {
+    confirmMsg.classList.remove('email-confirm-on');
+  }, 1200);
 }
